d80e3c3630c0fcea19a38205beb151b65577e364
Rafael Roman <panthro.rafael@gmail.com>
2017-02-22 20:13:42 +0100

    support custom cdn for downloading consul (#30)
    * support custom cdn for downloading consul
    
    * trying to use trusty to avoid tsl errors as per:
    https://github.com/travis-ci/travis-ci/issues/7131

--- src/main/groovy/com/pszymczyk/consul/infrastructure/HttpBinaryRepository.groovy
+++ src/main/groovy/com/pszymczyk/consul/infrastructure/HttpBinaryRepository.groovy
@@ -2,9 +2,14 @@ package com.pszymczyk.consul.infrastructure
 
 class HttpBinaryRepository {
 
+
+    public static final String CONSUL_BINARY_CDN = "CONSUL_BINARY_CDN"
+    public static final String CONSUL_DEFAULT_CDN = "https://releases.hashicorp.com/consul/"
+
     File getConsulBinaryArchive(String version, File file) {
         String os = OsResolver.resolve()
-        String url = "https://releases.hashicorp.com/consul/${version}/consul_${version}_${os}_amd64.zip"
+        String cdn = System.getenv(CONSUL_BINARY_CDN) != null ? System.getenv(CONSUL_BINARY_CDN) : CONSUL_DEFAULT_CDN;
+        String url = "${cdn}${version}/consul_${version}_${os}_amd64.zip"
         OutputStream outputStream = file.newOutputStream()
         outputStream << new URL(url).openStream()
         outputStream.close()
