623f55998c8a0565db1456b76833a93caa3bd8e7
Pawel Szymczyk <pawel@pszymczyk.com>
2019-08-28 13:59:51 +0200

    fix build

--- src/test/groovy/com/pszymczyk/consul/ConsulTestWaiter.groovy
+++ src/test/groovy/com/pszymczyk/consul/ConsulTestWaiter.groovy
@@ -6,6 +6,7 @@ import com.ecwid.consul.v1.catalog.CatalogServicesRequest
 import com.pszymczyk.consul.infrastructure.ConsulWaiter
 import com.pszymczyk.consul.infrastructure.client.ConsulClientFactory
 
+import java.util.concurrent.Callable
 import java.util.concurrent.TimeUnit
 
 import static com.jayway.awaitility.Awaitility.await
@@ -21,16 +22,16 @@ class ConsulTestWaiter extends ConsulWaiter {
     }
 
     void awaitUntilServiceRegistered(String id) {
-        await().atMost(30, TimeUnit.SECONDS).until({
-            assert consulClient.getAgentServices().getValue().values().findAll({ id == it.id }).size() == 1
+        await().atMost(30, TimeUnit.SECONDS).until((Callable<Boolean>) {
+            consulClient.getAgentServices().getValue().values().findAll({ id == it.id }).size() == 1
         })
     }
 
     void awaitConsulServiceRegistered(Optional<String> token) {
-        await().atMost(30, TimeUnit.SECONDS).until({
+        await().atMost(30, TimeUnit.SECONDS).until((Callable<Boolean>) {
             def builder = CatalogServicesRequest.newBuilder().setQueryParams(QueryParams.DEFAULT)
             token.ifPresent({ builder.setToken(it) })
-            assert consulClient.getCatalogServices(builder.build()).getValue().keySet().contains("consul")
+            consulClient.getCatalogServices(builder.build()).getValue().keySet().contains("consul")
         })
     }
 }
