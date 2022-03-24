026bc829f82b4f12fba5ed7d6a857136aa2f09ab
shijun_deng <dengshijun1992@qq.com>
2022-03-24 17:46:45 +0800

    support https

--- src/main/groovy/com/pszymczyk/consul/ConsulPorts.groovy
+++ src/main/groovy/com/pszymczyk/consul/ConsulPorts.groovy
@@ -5,14 +5,16 @@ import com.pszymczyk.consul.infrastructure.Ports
 class ConsulPorts {
 
     final int httpPort
+    final int httpsPort
     final int dnsPort
     final int serfLanPort
     final int serfWanPort
     final int serverPort
     final int grpcPort
 
-    private ConsulPorts(int httpPort, int dnsPort, int serfLanPort, int serfWanPort, int serverPort, int grpcPort) {
+    private ConsulPorts(int httpPort,int httpsPort, int dnsPort, int serfLanPort, int serfWanPort, int serverPort, int grpcPort) {
         this.httpPort = httpPort
+        this.httpsPort = httpsPort
         this.dnsPort = dnsPort
         this.serfLanPort = serfLanPort
         this.serfWanPort = serfWanPort
@@ -27,6 +29,7 @@ class ConsulPorts {
     static class ConsulPortsBuilder {
 
         private int httpPort = -1
+        private int httpsPort = -1
         private int dnsPort = -1
         private int serfLanPort = -1
         private int serfWanPort = -1
@@ -39,6 +42,7 @@ class ConsulPorts {
         ConsulPorts build() {
             return new ConsulPorts(
                     randomIfNotSet(httpPort),
+                    randomIfNotSet(httpsPort),
                     randomIfNotSet(dnsPort),
                     randomIfNotSet(serfLanPort),
                     randomIfNotSet(serfWanPort),
@@ -53,6 +57,11 @@ class ConsulPorts {
 
         ConsulPortsBuilder withHttpPort(int httpPort) {
             this.httpPort = httpPort
+            return this
+        }
+        
+        ConsulPortsBuilder withHttpsPort(int httpsPort) {
+            this.httpsPort = httpsPort
             return this
         }
 
@@ -83,6 +92,7 @@ class ConsulPorts {
 
         ConsulPortsBuilder fromConsulPorts(ConsulPorts consulPorts) {
             this.httpPort = consulPorts.httpPort
+            this.httpsPort = consulPorts.httpsPort
             this.dnsPort = consulPorts.dnsPort
             this.serfLanPort = consulPorts.serfLanPort
             this.serfWanPort = consulPorts.serfWanPort
